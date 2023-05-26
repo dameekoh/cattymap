dc8eaf633a7f5a0235f76f95c6511e3b11418992
20210770 <punnlert@kaist.ac.kr>
2023-05-27 03:10:58 +0900

    disabled the default UI

--- src/lib/GoogleMap.svelte
+++ src/lib/GoogleMap.svelte
@@ -13,6 +13,7 @@
       map = new google.maps.Map(mapElement, {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
+        disableDefaultUI: true,
         options: {
           styles: [
             {
