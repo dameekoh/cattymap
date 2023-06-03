d993265fa9432b668b4682410aac8c1d6d0b0a91
zhilin <zhilin_yap@yahoo.com>
2023-06-03 14:01:25 +0900

    for pull

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script>
-      import GoogleMap from '$lib/GoogleMap.svelte';
-      import LogIn from '$lib/LogIn.svelte';
+    import GoogleMap from '$lib/GoogleMap.svelte';
+    import LogIn from '$lib/LogIn.svelte';
 </script>
 
-    <div class="container">
-      <div class="flex flex-col items-center justify-center min-h-screen">
-        <LogIn />
-<<<<<<< HEAD
-=======
-        <!-- <GoogleMap /> -->
->>>>>>> 1f8ca803b0b8e0669f22b686c7f425af75b0803f
-      </div>
+  <div class="container">
+    <div class="flex flex-col items-center justify-center min-h-screen">
+      <LogIn />
     </div>
-    
+  </div>
