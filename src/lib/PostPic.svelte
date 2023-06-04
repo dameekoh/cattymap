cf8d531cb09e662f64584acbdbeab72218f2bec5
zhilin <zhilin_yap@yahoo.com>
2023-06-04 10:54:04 +0900

    Add PostPic.svelte

---
+++ src/lib/PostPic.svelte
@@ -0,0 +1,13 @@
+<script>
+    import LedgerProfile from './LedgerProfile.svelte';
+</script>
+
+
+
+
+<div class="card fixed left-1 shadow-xl p-3 ml-7 space-y-2 bg-white items-left">
+    <h2 class="card-title text-sm text-slate-500">Cats</h2>
+    <LedgerProfile profilePic = "https://cdn.iconscout.com/icon/premium/png-512-thumb/american-shorthair-1975261-1664591.png?f=avif&w=256" name = "Damir"/>
+    <LedgerProfile profilePic = "https://cdn.iconscout.com/icon/premium/png-512-thumb/abyssinnian-cat-1975262-1664592.png?f=avif&w=256" name = "Zhilin"/>
+    <LedgerProfile profilePic = "https://cdn.iconscout.com/icon/premium/png-512-thumb/nebelung-1975276-1664606.png?f=avif&w=256" name = "Punn" />
+  </div>
