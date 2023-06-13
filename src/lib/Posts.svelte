cb32498a2f70877ed3bf5581fb81a17ea0b11194
zhilin <zhilin_yap@yahoo.com>
2023-06-13 14:48:50 +0900

    trial 3

--- src/lib/Posts.svelte
+++ src/lib/Posts.svelte
@@ -128,17 +128,6 @@
         const mergedArray = [...comments.map(item => ({ ...item, type: 'comment' })), ...userComments.map(item => ({ ...item, type: 'userComment' }))];
         sortedComments = mergedArray.sort((a, b) => a.timestamp - b.timestamp);
     } 
-    function listenForCommentUpdates() {
-        onValue(commentRef, async (snapshot) => {
-        const commentDB = snapshot.val();
-        // Update the comments based on the updated commentDB
-        await getOwnerComments();
-        await getOtherComments();
-        arrangeComments();
-     }, (error) => {
-        console.error("Error listening for comment updates:", error);
-    });
-}
 
     function timeAgo(timestamp) {
         const now = Date.now();
@@ -234,7 +223,6 @@
         await getOwnerComments();
         await getOtherComments();
         arrangeComments();
-        listenForCommentUpdates();
     })
 
     // Check if SpeechRecognition is available
