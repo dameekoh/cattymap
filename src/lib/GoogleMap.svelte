882eb27f70dc58fe5dfcfa747f37dd8aa1b2438d
zhilin <zhilin_yap@yahoo.com>
2023-06-14 19:09:29 +0900

    fix deploy i guess

--- src/lib/GoogleMap.svelte
+++ src/lib/GoogleMap.svelte
@@ -469,7 +469,6 @@ async function proposeCat() {
         {#each catProfilesonLegend as { name, avatar }}
           <LedgerProfile avatar = { avatar } name = { name } on:filterChange = {displayCatMarkers}/>
         {/each}
-        <LedgerProfile isCat = {false} on:filterChange = {displayCatMarkers}/>
       </div>
       <div class="button__container">
         <button class="btn btn-xs btn-block btn-active btn-secondary" style="line-height: auto;" on:click={proposeCat}>
